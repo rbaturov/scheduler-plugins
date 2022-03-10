88c25b8b36ac0b8fbf5e545f3756cbf1a4852456
Francesco Romani <fromani@redhat.com>
2022-03-10 10:31:32 +0100

    [KNI] align kni-local tests to u/s
    upstream improved how integration tests are run;
    adapt our local infra to be up to speed.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack-kni/integration-test-quick.sh
+++ hack-kni/integration-test-quick.sh
@@ -21,45 +21,24 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")/..
 source "${SCRIPT_ROOT}/hack/lib/init.sh"
 
-checkEtcdOnPath() {
-  # If it's in a prow CI env, add etcd to path.
-  [[ ${CI:-} == "true" ]] && export PATH="$(pwd)/etcd:${PATH}"
-  kube::log::status "Checking etcd is on PATH"
-  command -v etcd >/dev/null && return
-  kube::log::status "Cannot find etcd, cannot run integration tests."
-  kube::log::status "Please see https://git.k8s.io/community/contributors/devel/sig-testing/integration-tests.md#install-etcd-dependency for instructions."
-  # kube::log::usage "You can use 'hack/install-etcd.sh' to install a copy in third_party/."
-  return 1
-}
+TEMP_DIR=${TMPDIR-/tmp}
 
-CLEANUP_REQUIRED=
 cleanup() {
-  [[ -z "${CLEANUP_REQUIRED}" ]] && return
-  kube::log::status "Cleaning up etcd"
-  kube::etcd::cleanup
-  CLEANUP_REQUIRED=
-  kube::log::status "Integration test cleanup complete"
+  rm -rf "${TEMP_DIR}/setup-envtest"
 }
 
 runTests() {
-  kube::log::status "Starting etcd instance"
-  CLEANUP_REQUIRED=1
-  kube::etcd::start
+  kube::log::status "Configuring envtest"
+  source "${TEMP_DIR}/setup-envtest"
+
   kube::log::status "Running integration test cases"
-
-  ln -s ../../../../../../../hack/testdata vendor/k8s.io/kubernetes/cmd/kube-apiserver/app/testing/testdata
   # TODO: make args customizable.
-  GOFLAGS="-v" go test \
-  test/integration/noderesourcetopology_test.go \
-  test/integration/base.go \
-  test/integration/main_test.go -v
-
-  cleanup
+  go test -timeout=40m -mod=vendor -v \
+	  test/integration/noderesourcetopology_test.go \
+	  test/integration/utils.go \
+	  test/integration/main_test.go
 }
 
-checkEtcdOnPath
-
-# Run cleanup to stop etcd on interrupt or other kill signal.
 trap cleanup EXIT
 
 runTests
