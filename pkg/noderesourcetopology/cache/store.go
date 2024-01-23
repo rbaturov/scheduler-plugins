cf474357379979d5dbd9988ac9ed7406e3a50160
Francesco Romani <fromani@redhat.com>
2024-01-23 18:42:01 +0100

    Revert "[KNI][downstream] nrt: add optional kni-specific informer"
    This reverts commit 39321ed9d0c0dc547d45a3c7d6963a17fbd5279d.

--- pkg/noderesourcetopology/cache/store.go
+++ pkg/noderesourcetopology/cache/store.go
@@ -34,8 +34,6 @@ import (
 	"sigs.k8s.io/scheduler-plugins/pkg/util"
 
 	"github.com/k8stopologyawareschedwg/podfingerprint"
-
-	kniinformer "sigs.k8s.io/scheduler-plugins/pkg-kni/podinformer"
 )
 
 // nrtStore maps the NRT data by node name. It is not thread safe and needs to be protected by a lock.
@@ -256,7 +254,7 @@ func makeNodeToPodDataMap(podLister podlisterv1.PodLister, logID string) (map[st
 		return nodeToObjsMap, err
 	}
 	for _, pod := range pods {
-		if !kniinformer.IsPodRelevantForState(pod) {
+		if pod.Status.Phase != corev1.PodRunning {
 			// we are interested only about nodes which consume resources
 			continue
 		}
